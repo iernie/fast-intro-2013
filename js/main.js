4ea005b9732cc9ff8f323e8062965713289c3cc5
haeric <hakon.erichsen@gmail.com>
2013-03-29 17:24:05 +0100

    explosions

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var VIEW_ANGLE = 45,
 var clock;
 
 var camera, scene, renderer;
-var pointLight;
+var pointLight, geishaObj;
 
 var map = [
 	"         bbb         ",
@@ -21,6 +21,8 @@ var map = [
 	" bbbbbbbbbbbbbbbbbbb ",
 ]
 
+
+
 materials = {
 	'x': new THREE.MeshLambertMaterial({color: 0x632c1c}),
 	'=': new THREE.MeshLambertMaterial({color: 0xde8342}),
@@ -29,6 +31,11 @@ materials = {
 	'@': new THREE.MeshLambertMaterial({color: 0x3a6fa4}),
 	'#': new THREE.MeshLambertMaterial({color: 0x4e76a4}),
 	'b': new THREE.MeshLambertMaterial({color: 0xE8DC44}),
+	'g': new THREE.MeshLambertMaterial({
+	  color     : 0xcccccc,
+	  shininess : 200,
+	  shading   : THREE.SmoothShading,
+	}),
 }
 
 init();
@@ -77,6 +84,7 @@ function init() {
 	// Geisha
 	//
 	var counter = 0;
+	geishaObj = [];
 
 	for (var y = 0; y < geisha.length; y++) {
 		for (var x = 0; x < geisha[y].length; x++) {
@@ -89,6 +97,7 @@ function init() {
 				mesh.position.x = x-100;
 				mesh.position.y = 80;
 				scene.add(mesh);
+				geishaObj.push(mesh);
 			}
 		}
 	}
@@ -148,24 +157,62 @@ function animate() {
 		}
 	}
 
-	if ( time < 10 ) {
+	var goatPosition = new THREE.Vector3(0, 0, 0);
+
+	if ( time < 11 ) {
 		camera.position.y = 20*Math.sin(time);
 		camera.position.x = 50*Math.cos(time);
 		camera.position.z = 50*Math.sin(time);
 		camera.lookAt(new THREE.Vector3(Math.sin(time)*10, 0, 0));
 	}
-	else if ( time < 25 ) {
+	else if ( time < 28 ) {
 		camera.position.y = 20*Math.sin(time)+20;
 		camera.position.x = 50*Math.cos(time)-100;
 		camera.position.z = 50*Math.sin(time);
 		camera.lookAt(new THREE.Vector3(Math.sin(time)*10-80, -20, 0));	
 	}
 
-	else if ( time < 50 ) {
+	else if ( time < 30 ) {
 		camera.position.y = -20;
 		camera.position.x = -70;
 		camera.position.z = 170;
 		camera.lookAt(new THREE.Vector3(-70, -20, 0));	
+	}
+	else if ( time < 31 ) {
+		$('#countdown3').show();
+	}
+	else if ( time < 32 ) {
+		$('#countdown3').hide();
+		$('#countdown2').show();
+	}
+	else if ( time < 33 ) {
+		$('#countdown2').hide();
+		$('#countdown1').show();
+	}
+	else if ( time < 34 ) {
+		$('#countdown1').hide();
+		$('#fight').show();
+	}
+	else if ( time < 38 ) {
+		$('#fight').hide();
+	}
+	else if ( time < 43 ) {
+		for (var i = 0; i < geishaObj.length; i++) {
+			geishaObj[i].position.x += 0.3;
+		}
+	}
+	else if ( time < 60 ) {
+		for (var i = 0; i < geishaObj.length; i++) {
+			var obj = geishaObj[i];
+			var len = obj.position.distanceTo(goatPosition);
+
+			if ( !obj.vec && len < time-60) {
+				obj.vec = new THREE.Vector3(Math.random(), Math.random(), Math.random());
+			}
+			if ( obj.vec ) {
+				obj.position.add(obj.vec);
+			}
+		}
 	}
 
 	renderer.render(scene, camera);
